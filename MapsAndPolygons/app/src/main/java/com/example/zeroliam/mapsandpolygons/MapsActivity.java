acf761432a44448fdf53059ec8adcb5b521eddfc
saquib-4414 <justnazmus@gmail.com>
2017-05-02 20:22:48 +0200

    map added+textbox and search activated

--- MapsAndPolygons/app/src/main/java/com/example/zeroliam/mapsandpolygons/MapsActivity.java
+++ MapsAndPolygons/app/src/main/java/com/example/zeroliam/mapsandpolygons/MapsActivity.java
@@ -1,14 +1,26 @@
 package com.example.zeroliam.mapsandpolygons;
 
+import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -18,11 +30,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        if(googleServicesAvailable()){
+            Toast.makeText(this, "Perfect", Toast.LENGTH_SHORT).show();
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+    public boolean googleServicesAvailable()
+    {
+        GoogleApiAvailability api=GoogleApiAvailability.getInstance();
+        int isAvailable=api.isGooglePlayServicesAvailable(this);
+        if(isAvailable== ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if(api.isUserResolvableError(isAvailable)){
+            Dialog dialog= api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "can't connect to google service", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 
 
     /**
@@ -37,10 +70,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        goToLocationZoom(50.9816511,11.3173627,20);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    private void goToLocation(double lat, double lng)
+    {
+        LatLng ll=new LatLng(lat,lng);
+        CameraUpdate update= CameraUpdateFactory.newLatLng(ll);
+        mMap.moveCamera(update);
+    }
+    private void goToLocationZoom(double lat, double lng, float zoom)
+    {
+        LatLng ll=new LatLng(lat,lng);
+        CameraUpdate update= CameraUpdateFactory.newLatLngZoom(ll,zoom);
+        mMap.moveCamera(update);
+    }
+    public void geoLocate(View view) throws IOException {
+        EditText et=(EditText) findViewById(R.id.editText);
+        String location= et.getText().toString();
+        Geocoder gc = new Geocoder(this);
+       List<Address> list= gc.getFromLocationName(location,1);
+        Address address=list.get(0);
+        String locality=address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat=address.getLatitude();
+        double lng=address.getLongitude();
+        goToLocationZoom(lat,lng,20);
+    }
 }
+
